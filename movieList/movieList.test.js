46c771636b289797a230fe865872ae885eca2e11
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-20 13:09:58 -0700

    added beforeAll and afterAll

--- movieList/movieList.test.js
+++ movieList/movieList.test.js
@@ -3,3 +3,11 @@ const {Builder, Capabilities} = require('selenium-webdriver')
 require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeAll(async () => {
+    await (await driver).get('http://localhost:5500/exercises/automation/movieList/index.html')
+})
+
+afterAll(async () => {
+    await (await driver).quit()
+})
