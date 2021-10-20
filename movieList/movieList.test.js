b2b3a1bb0390e92960f7b406734c3896176346d1
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-20 15:04:40 -0700

    passed addMovie test

--- movieList/movieList.test.js
+++ movieList/movieList.test.js
@@ -1,13 +1,40 @@
-const {Builder, Capabilities} = require('selenium-webdriver')
+const {Builder, Capabilities, By} = require('selenium-webdriver')
 
 require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeAll(async () => {
-    await (await driver).get('http://localhost:5500/exercises/automation/movieList/index.html')
+    await (await driver).get('http://127.0.0.1:5500/automation-b/movieList/index.html')
 })
 
 afterAll(async () => {
     await (await driver).quit()
 })
+
+test('Movie List', async () => {
+    // await search(driver, 'Add Movie')
+
+    const addMovie = driver.findElement(By.id('movie-input'))
+    console.log(addMovie)
+
+    await addMovie.sendKeys('Lion King')
+
+    await driver.findElement(By.name('button')).click()
+
+    await driver.sleep(3000)
+
+})
+// Write a test that adds a movie to the page
+// You’ll need to select the input field and button
+
+// You’ll need to type in the input field
+
+// You’ll need to click the button
+
+//css("[type='input']")
+//xpath("//form/input[text()='Add Movie'][1])"
+//name('input')
+//name('form')
+//By.css("[name='input']"
+//xpath("//*[contains(text(), 'form')]"))
